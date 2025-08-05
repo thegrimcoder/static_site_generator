7b1f92fdbef37bc8aa2ed0787c602f2ccf82ad32
thegrimcoder <mbrust035@gmail.com>
2025-08-04 20:25:10 -0500

    Created unittesting

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-print("hello world")
+
+from textnode import TextNode
+
+def main():
+    test_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(test_node)
+
+if __name__ == "__main__":
+    main()
 
