9e3000f476a7aad049fc228bcbf53b036fa8a709
thegrimcoder <mbrust035@gmail.com>
2025-08-06 17:22:10 -0500

    testing some more

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,7 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(self, tag, value, props)
+        super().__init__(self, tag, value, chlidren=None, props)
 
     def to_html(self):
         if not self.value:
