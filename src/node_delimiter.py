23a8ff3133eaa43ba7a5d170a77df08a002c3833
thegrimcoder <mbrust035@gmail.com>
2025-08-16 15:13:50 -0500

    testing

--- src/node_delimiter.py
+++ src/node_delimiter.py
@@ -8,9 +8,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     for node in old_nodes:
         if node.text_type == TextType.TEXT:
-            split_strings = node.split(delimiter, -1)
+            split_strings = node.text.split(delimiter, -1)
+            if len(split_strings) <= 2:
+                raise Exception("Matching Delimiter Not Found")            
+            text_string = True
             for string in split_strings:
-                print(string)
+                if text_string == True:
+                    new_nodes.append(TextNode(string, TextType(node.text_type)))
+                    text_string = False
+                else:
+                    new_nodes.append(TextNode(string, TextType(text_type)))
+                    text_string = True
         else:
             new_nodes.extend(node)
     return new_nodes
---
+++ src/test_node_delimiter.py
@@ -0,0 +1,30 @@
+import unittest
+
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+from parentnode import ParentNode
+from node_delimiter import split_nodes_delimiter
+
+class TestNodeDelimiter(unittest.TestCase):
+    def test_node_delimiter_text(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        print(new_nodes)
+
+    def test_node_delimiter_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        print(new_nodes)
+
+    def test_node_delimiter_italic(self):
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        print(new_nodes)
+        
+    def test_node_delimiter_no_delimiter(self):
+        node = TextNode("This is text with an _italic word", TextType.TEXT)
+        self.assertRaises(Exception, split_nodes_delimiter([node], "_", TextType.ITALIC))
+
+if __name__ == "__main__":
+    unittest.main()
