a41164e43f524fa64bf9e8c66dab646eb05d5e28
thegrimcoder <mbrust035@gmail.com>
2025-08-18 17:54:05 -0500

    create split_nodes.py

---
+++ src/split_nodes.py
@@ -0,0 +1,13 @@
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+from parentnode import ParentNode
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        pass
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
