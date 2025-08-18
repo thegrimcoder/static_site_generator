0620bc705a1fc24d7f5aa7b650c481b1d3ba6611
thegrimcoder <mbrust035@gmail.com>
2025-08-17 20:22:03 -0500

    completed extract_markdown

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -2,4 +2,10 @@ from textnode import TextNode, TextType
 from htmlnode import HTMLNode
 from leafnode import LeafNode
 from parentnode import ParentNode
+import re
 
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
---
+++ src/test_extract_markdown.py
@@ -0,0 +1,23 @@
+import unittest
+
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+from parentnode import ParentNode
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+if __name__ == "__main__":
+    unittest.main()
