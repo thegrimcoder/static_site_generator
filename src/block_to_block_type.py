f05eee5fd1d0aa59594fa9fc0f8851694585fc27
thegrimcoder <mbrust035@gmail.com>
2025-08-25 18:07:25 -0500

    BlockType enum and block_to_block_type

---
+++ src/block_to_block_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block):
+    pass
