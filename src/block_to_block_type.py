0273fa844e4947fd4da6a7064f650bb8bd6b620d
thegrimcoder <mbrust035@gmail.com>
2025-08-26 17:29:07 -0500

    block type

--- src/block_to_block_type.py
+++ src/block_to_block_type.py
@@ -9,4 +9,28 @@ class BlockType(Enum):
     ORDERED_LIST = "ordered_list"
 
 def block_to_block_type(block):
-    pass
+    test_string = ""
+    temp_block = block
+    block_lines = block.splitlines()
+
+    for char in temp_block:
+        test_string += char
+
+        if test_string == "#" or test_string == "##" or test_string == "###" or test_string == "####" or test_string == "#####":
+            continue
+
+        if test_string == "# " or test_string == "## " or test_string == "### " or test_string == "#### " or test_string == "##### " or test_string == "###### ":
+            return BlockType.HEADING
+        
+        if test_string == "`" or test_string == "``":
+            continue
+
+        if test_string == "```" and block[-3:] == "```":
+            return BlockType.CODE
+        
+
+    for line in block_lines:
+        if line:
+            first_char = line[0]
+        
+        
